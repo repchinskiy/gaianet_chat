78eb992e4385f63e49fd4415e4dfcd55d87d37ca
repchinskiy <repchinskiy@gmail.com>
2025-01-15 17:06:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ sed -i 's/8080/8383/g' ~/gaianet/config.json
 sed -i 's/8080/8383/g' ~/gaianet/gaia-frp/frpc.toml
 sed -i 's/8080/8383/g' ~/gaianet/dashboard/config_pub.json
 gaianet start
+gaianet stop
+gaianet --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+gaianet start
 source ~/checker_tg/venv/bin/activate
 pip install -r requirements.txt
 #python3 gaia_bot.py url
