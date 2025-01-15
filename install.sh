5592844a82522624321a50640a7405301b2a5af0
repchinskiy <repchinskiy@gmail.com>
2025-01-15 16:55:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 apt install curl
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
 source ~/.bashrc
-gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json  
+sleep 1
+gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 sudo ufw allow 8383
 sed -i 's/8080/8383/g' ~/gaianet/config.json
 sed -i 's/8080/8383/g' ~/gaianet/gaia-frp/frpc.toml
