a50a2062744cf18d734242563d811c3113a4a304
repchinskiy <repchinskiy@gmail.com>
2025-01-15 17:26:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 apt install curl
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
-sleep 1
-source /root/.bashrc
-sleep 1
-source /root/.bashrc
-sleep 1
+export PATH="/root/gaianet/bin:$PATH"
+source "/root/.wasmedge/env"
 gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 sudo ufw allow 8383
 sed -i 's/8080/8383/g' ~/gaianet/config.json
